f67671cd31dc35f4bdb30ac3e5d743612799f83c
WiaciWalls <vyachproz7@gmail.com>
2023-09-20 08:28:31 +0300

    parser

--- start.py
+++ start.py
@@ -4,8 +4,16 @@ import re
 
 base = []
 path = "https://losyash-library.fandom.com/ru/wiki/Скамейка"
+#path = "https://losyash-library.fandom.com/ru/wiki/День_учителя"
 
-for i in range(50):
+
+def clean(bs):
+    for i in range(len(bs) - 1, -1, -1):
+        if bs[i].text[0] == '(' or bs[i].text[0] == '*':
+            bs.remove(bs[i])
+    return bs
+
+for i in range(297):
     r = requests.get(path)
     soup = BeautifulSoup(r.text)
 
@@ -16,16 +24,20 @@ for i in range(50):
 
     for cite in cites:
         bs = cite.select("b")
+        bs = clean(bs)
+
         i += 1
-        reg = ': |'.join([b.text for b in bs]) + ': '
+        reg = ': |'.join([b.text.replace('(', '\(').replace(')', '\)') for b in bs]) + ': '
         parts = re.split(reg, cite.text)
         
         if len(bs) == 0:
+            if len(s) <= used:
+                continue
             base.append([s[used].text[2:], parts[0]])
             used += 1
         else:
             for j in range(len(parts) - 1):
-                base.append([bs[j].text, parts[j + 1]])
+                base.append([bs[j].text, parts[j + 1].replace('\n\n', '')])
 
     url = "" + soup.select("table.wikitable td[style] > a")[-1]['href']
     if url == "":
